bcbeb254da71bb73561d8bdfaca28a0db9e84f1a
devpatcar <patrik.carlsson@apirix.com>
2017-10-09 14:49:29 +0200

    remove added

--- src/pages/admin/admin.ts
+++ src/pages/admin/admin.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import firebase from 'firebase';
 import { CoursePage} from '../course/course';
 
@@ -13,12 +13,13 @@ export class AdminPage {
   private _db: any;
   private _coursesRef: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) { 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) { 
     let that = this;   
     this._db = firebase.database().ref('/');
     this._coursesRef = firebase.database().ref('courses');   
     this._coursesRef.on("child_added", function(snapshot, prevChildKey) {      
-      var data = snapshot.val(); 
+      var data = snapshot.val();       
+      data.key = snapshot.key;
       that.courses.push(data); 
     });   
   }
@@ -29,6 +30,37 @@ export class AdminPage {
  
   createCourse(){
     this.navCtrl.push(CoursePage);
+  }
+
+  removeCourse(course){      
+    const alert = this.alertCtrl.create({
+      title: 'Confirm delete',     
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            
+          }
+        },
+        {
+          text: 'Remove',
+          handler: () => {            
+            this._coursesRef.child(course.key).remove();    
+            let index = this.courses.indexOf(course);
+            
+                   if(index > -1){
+                       this.courses.splice(index, 1);
+                   }       
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  editCourse(){
+    
   }
 
   logOut(){
